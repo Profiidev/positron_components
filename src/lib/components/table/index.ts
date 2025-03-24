d2a1723cb2314f7e1333cc6384b81aea664d9f1b
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2025-03-24 21:04:49 +0100

    fix: item and group multiselect export missing

--- src/lib/components/table/index.ts
+++ src/lib/components/table/index.ts
@@ -1,8 +1,19 @@
 import Actions from './actions.svelte';
-import Multiselect from './multiselect.svelte';
+import Multiselect, {
+  type Group as MultiselectGroup,
+  type Item as MultiselectItem
+} from './multiselect.svelte';
 import SimpleTable from './simple-table.svelte';
 import TableHead from './table-head.svelte';
 import BaseTable from './base-table.svelte';
 export * from './helpers.svelte.js';
 
-export { Actions, Multiselect, SimpleTable, TableHead, BaseTable };
+export {
+  Actions,
+  Multiselect,
+  type MultiselectGroup,
+  type MultiselectItem,
+  SimpleTable,
+  TableHead,
+  BaseTable
+};
