fe93f792eaa7393bed7d47db82ab152beb5f403f
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2025-06-11 20:50:17 +0200

    fix: moved deps to non dev

--- src/lib/util/time.svelte.ts
+++ src/lib/util/time.svelte.ts
@@ -1,7 +1,7 @@
 import { browser } from '$app/environment';
-import type { DateTime as DateTimeT } from 'luxon';
+import type { DateTime as DateTimeType } from 'luxon';
 
-export let DateTime: typeof DateTimeT;
+export let DateTime: typeof DateTimeType;
 
 if (browser) {
   import('luxon').then((luxon) => (DateTime = luxon.DateTime));
