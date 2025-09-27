d2a91e68e49944475f651579ae604de53b723bda
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2025-09-27 13:39:53 +0200

    feat: hideable input

---
+++ src/lib/components/form/hideable-input.svelte
@@ -0,0 +1,23 @@
+<script lang="ts" generics="S extends FormRecord = FormRecord">
+  import type { FormRecord, SuperForm, FormPath } from '$lib/components/form';
+  import { Input, Label } from '$lib/components/ui';
+  import type { Snippet } from 'svelte';
+
+  interface Props {
+    formData: SuperForm<S>;
+    key: FormPath<S>;
+    id: string;
+    value: any;
+    children: Snippet;
+  }
+
+  const { formData: form, key, id, value, children }: Props = $props();
+  let { form: formData } = $derived(form);
+</script>
+
+{#if $formData[key]}
+  <Label for={id} class="flex">
+    {@render children()}
+  </Label>
+  <Input {id} {value} readonly />
+{/if}
