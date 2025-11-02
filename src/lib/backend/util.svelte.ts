475fd296bdd5289a663e0b80dd900784814fdd12
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2025-11-02 23:01:48 +0100

    feat: better request methods

--- src/lib/backend/util.svelte.ts
+++ src/lib/backend/util.svelte.ts
@@ -1,43 +1,84 @@
-import { ContentType, RequestError, ResponseType } from './types.svelte';
+import { RequestError, ResponseType } from './types.svelte';
+
+export const patch = async <T>(
+  path: string,
+  res_type: ResponseType,
+  body: any,
+  content_type?: string,
+  signal?: AbortSignal
+): Promise<T | RequestError> => {
+  return await request(path, 'PATCH', res_type, body, content_type, signal);
+};
+
+export const put = async <T>(
+  path: string,
+  res_type: ResponseType,
+  body: any,
+  content_type?: string,
+  signal?: AbortSignal
+): Promise<T | RequestError> => {
+  return await request(path, 'PUT', res_type, body, content_type, signal);
+};
+
+export const delete_ = async <T>(
+  path: string,
+  res_type: ResponseType,
+  body: any,
+  content_type?: string,
+  signal?: AbortSignal
+): Promise<T | RequestError> => {
+  return await request(path, 'DELETE', res_type, body, content_type, signal);
+};
 
 export const post = async <T>(
   path: string,
   res_type: ResponseType,
-  content_type: ContentType,
   body: any,
+  content_type?: string,
   signal?: AbortSignal
 ): Promise<T | RequestError> => {
-  return await request(path, 'POST', res_type, content_type, body, signal);
+  return await request(path, 'POST', res_type, body, content_type, signal);
 };
 
 export const get = async <T>(
   path: string,
   res_type: ResponseType,
+  content_type?: string,
   signal?: AbortSignal
 ): Promise<T | RequestError> => {
-  return await request(path, 'GET', res_type, undefined, undefined, signal);
+  return await request(path, 'GET', res_type, undefined, content_type, signal);
 };
 
-const request = async <T>(
+export const request = async <T>(
   path: string,
   method: string,
   res_type: ResponseType,
-  content_type?: ContentType,
   body?: any,
+  content_type?: string,
   signal?: AbortSignal
 ): Promise<T | RequestError> => {
-  let headers;
-  if (content_type && body) {
-    headers = {
-      'Content-Type': content_type
-    };
+  let body_data = body;
+
+  if (body instanceof ArrayBuffer) {
+    content_type = 'application/octet-stream';
+  } else if (body instanceof Blob) {
+    content_type = body.type;
+  } else if (typeof body === 'string') {
+    content_type = 'text/plain';
+  } else if (typeof body === 'object' && body !== null) {
+    content_type = 'application/json';
+  }
+
+  let headers: HeadersInit = {};
+  if (content_type) {
+    headers['Content-Type'] = content_type;
   }
 
   try {
     let res = await fetch!(`${path}`, {
       method,
       headers,
-      body,
+      body: body_data,
       signal
     });
 
