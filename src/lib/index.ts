413135a6aeec4dbfc8fbd19a269f08b4b846f917
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2025-11-26 17:13:04 +0100

    fix: better export strategy

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,1 @@
-// Reexport your entry components here
-export * from './backend/index.js';
-export * from './components/index.js';
-export * from './hooks/index.js';
-export * from './util/index.js';
-export * from './utils.js';
+// There are no root exports use the sub-modules
