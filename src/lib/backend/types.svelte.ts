026cac22b786aeab88fba5f959ad7cd86d0fd202
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2026-01-25 23:07:19 +0100

    feat: added new request error types

--- src/lib/backend/types.svelte.ts
+++ src/lib/backend/types.svelte.ts
@@ -1,8 +1,23 @@
 export enum RequestError {
-  Other = 'Other',
+  BadRequest = 'BadRequest',
   Unauthorized = 'Unauthorized',
+  Forbidden = 'Forbidden',
+  NotFound = 'NotFound',
+  NotAcceptable = 'NotAcceptable',
+  RequestTimeout = 'RequestTimeout',
   Conflict = 'Conflict',
-  Gone = 'Gone'
+  Gone = 'Gone',
+  ContentTooLarge = 'ContentTooLarge',
+  UnsupportedMediaType = 'UnsupportedMediaType',
+  UnprocessableEntity = 'UnprocessableEntity',
+  TooManyRequests = 'TooManyRequests',
+  InternalServerError = 'InternalServerError',
+  NotImplemented = 'NotImplemented',
+  BadGateway = 'BadGateway',
+  ServiceUnavailable = 'ServiceUnavailable',
+  GatewayTimeout = 'GatewayTimeout',
+  InsufficientStorage = 'InsufficientStorage',
+  Other = 'Other'
 }
 
 export enum ResponseType {
