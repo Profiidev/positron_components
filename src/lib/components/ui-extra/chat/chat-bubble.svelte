5745a5830b97f5702f0aaa40647aa155f02049d9
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2026-02-25 14:25:59 +0100

    chore: updated deps

--- src/lib/components/ui-extra/chat/chat-bubble.svelte
+++ src/lib/components/ui-extra/chat/chat-bubble.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
   import type { ChatBubbleProps } from '$lib/components/ui-extra/chat/types';
+  import { cn } from '../../../blocks/utils.js';
 
   let {
     ref = $bindable(null),
     variant,
     children,
+    class: className,
     ...rest
   }: ChatBubbleProps = $props();
 </script>
@@ -12,7 +14,10 @@
 <div
   {...rest}
   bind:this={ref}
-  class="group/chat-bubble flex max-w-[80%] flex-row place-items-end gap-2 data-[variant='sent']:place-self-end"
+  class={cn(
+    "group/chat-bubble flex max-w-[80%] flex-row place-items-end gap-2 data-[variant='sent']:place-self-end",
+    className
+  )}
   data-variant={variant}
 >
   {@render children?.()}
